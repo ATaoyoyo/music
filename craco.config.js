e06d6ce1e2dffde71cc98040ae002a62788796e4
ATaoyoyo <1029187998@qq.com>
2021-09-17 21:56:51 +0800

    tmd... don't coding...

--- craco.config.js
+++ craco.config.js
@@ -1,8 +1,30 @@
 const path = require('path')
+const cracoLess = require('craco-less')
 
 const resolve = (dirname) => path.join(__dirname, dirname)
 
 module.exports = {
+  style: {
+    postcss: {
+      mode: 'file',
+      plugins: [
+        require('postcss-pxtorem')({
+          rootValue: 37.5,
+          propList: ['*'],
+          selectorBlackList: ['.norem'], // 过滤掉.norem-开头的class，不进行rem转换
+        }),
+      ],
+      env: {
+        autoprefixer: {
+          /* Any autoprefixer options: https://github.com/postcss/autoprefixer#options */
+        },
+        stage: 3 /* Any valid stages: https://cssdb.org/#staging-process. */,
+        features: {
+          /* Any CSS features: https://preset-env.cssdb.org/features. */
+        },
+      },
+    },
+  },
   webpack: {
     alias: {
       '@': resolve('.'),
--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import ReactDom from 'react-dom'
+import 'lib-flexible/flexible'
 
 import 'normalize.css'
 import './style/index.less'
